b33d2ee02c94cf75771123686adf2c9e3f84e4f1
Taha-Hasemi <kiziltahaaslan@gmail.com>
2024-12-01 15:48:26 +0330

    add: newtonsoft json swagger auth jwt token

--- SmartHomeServer/Program.cs
+++ SmartHomeServer/Program.cs
@@ -1,15 +1,84 @@
 using MQTTnet;
 using MQTTnet.AspNetCore;
 using MQTTnet.Server;
+using TelegramBotAPI.BotConfiguration.Abstract;
+using TelegramBotAPI.BotConfiguration;
+using TelegramBotAPI.DependencyResolvers.Abstract;
+using TelegramBotAPI.DependencyResolvers;
+using TelegramBotAPI.Handle.Abstract;
+using TelegramBotAPI.Handle;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using Core.DependencyResolvers;
+using Core.Utilities.Security.Encryption;
+using Core.Utilities;
+using Core.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Core.Utilities.Security.Jwt;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddScoped<IBotConfiguration, BotConfiguration>();
+
+builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
+builder.Services.AddSingleton<IWebHostEnvironment>(builder.Environment);
+
+builder.Services.AddSingleton<IDependencyResolver, AutofacDR>();
+
+builder.Services.AddScoped<IHandle, UpdateHandle>();
+
+builder.Services.AddControllers().AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "JWTToken_Auth_API",
+        Version = "v1"
+    });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        {
+            new OpenApiSecurityScheme {
+                Reference = new OpenApiReference {
+                    Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
+
+var tokenOptions = builder.Configuration.GetSection("TokenOptions").Get<TokenOptions>()!;
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidIssuer = tokenOptions.Issuer,
+            ValidAudience = tokenOptions.Audience,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
+        };
+    });
+builder.Services.AddDependencyResolvers(new ICoreModule[] { new CoreModule() });
 
 builder.Services.AddHostedMqttServer(ob =>
 {
@@ -22,7 +91,7 @@ builder.WebHost.UseKestrel(
         o =>
         {
             o.ListenAnyIP(1883, l => l.UseMqtt());
-            o.ListenAnyIP(5001, l => l.UseHttps());
+            o.ListenAnyIP(5026, l => l.UseHttps());
         }
         );
 
