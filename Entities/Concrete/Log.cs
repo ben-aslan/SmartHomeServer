2f0265cbc473f3f606789e1f5db6a7107908a4e7
Taha-Hasemi <kiziltahaaslan@gmail.com>
2024-12-12 01:34:30 +0330

    add: device change: value case

--- Entities/Concrete/Log.cs
+++ Entities/Concrete/Log.cs
@@ -8,10 +8,12 @@ public class Log : IEntity
     [Key]
     public int Id { get; set; }
 
+    public string Device { get; set; } = null!;
+
     [Required]
     public string Name { get; set; } = null!;
 
-    public string vlaue { get; set; } = null!;
+    public string Value { get; set; } = null!;
 
     public DateTime CreateDate { get; set; } = DateTime.Now;
 
