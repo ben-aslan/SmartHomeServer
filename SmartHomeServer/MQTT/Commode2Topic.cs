4197a514a8533362c66e5a2e724041355b52b322
Taha-Hasemi <kiziltahaaslan@gmail.com>
2024-12-14 13:09:47 +0330

    add: commode2 topic

---
+++ SmartHomeServer/MQTT/Commode2Topic.cs
@@ -0,0 +1,37 @@
+﻿using Business.Abstract;
+using SmartHomeServer.MQTT.Abstract;
+using System.Text;
+using Telegram.Bot;
+
+namespace SmartHomeServer.MQTT;
+
+[Topic(Topic = "commode2")]
+public class Commode2Topic : ITopic
+{
+    ITelegramBotClient _client;
+    IStatService _statService;
+    ILogService _logService;
+    IUserService _userService;
+
+    public Commode2Topic(IBotService botService, IStatService statService, ILogService logService, IUserService userService)
+    {
+        _client = new TelegramBotClient(botService.GetSelectedBot().Token);
+        _statService = statService;
+        _logService = logService;
+        _userService = userService;
+    }
+
+    public void Execute(MQTTMessage message)
+    {
+        if (_statService.Any(message.Sender, message.Topic, Encoding.Default.GetString(message.Payload)))
+            return;
+        _userService.GetAdmins().ForEach(e =>
+        {
+            _client.SendMessage(e.ChatId, "commode 2 " + (Encoding.Default.GetString(message.Payload) == "0" ? "opended" : "closed"));
+        });
+
+        _statService.ChangeStatAsync(message.Sender, message.Topic, Encoding.Default.GetString(message.Payload));
+
+        _logService.AddAsync(message.Topic, Encoding.Default.GetString(message.Payload), message.Sender);
+    }
+}
