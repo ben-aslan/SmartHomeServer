a0f30e8d68370a22b7ecd9b9985aef9074542b74
Taha-Hasemi <kiziltahaaslan@gmail.com>
2024-12-27 13:45:53 +0330

    refactor: disabled alarm notification

--- SmartHomeServer/MQTT/AlarmTopic.cs
+++ SmartHomeServer/MQTT/AlarmTopic.cs
@@ -28,7 +28,7 @@ public class AlarmTopic : ITopic
             return;
         _userService.GetAdmins().ForEach(e =>
         {
-            _client.SendMessage(e.ChatId, "Alarm: " + (Encoding.Default.GetString(message.Payload) == "1" ? "activated" : "deactivated"));
+            _client.SendMessage(e.ChatId, "Alarm: " + (Encoding.Default.GetString(message.Payload) == "1" ? "activated" : "deactivated"), disableNotification: true);
         });
 
         _statService.ChangeStatAsync(((int)EClient.PublicClient).ToString(), message.Topic, Encoding.Default.GetString(message.Payload));
