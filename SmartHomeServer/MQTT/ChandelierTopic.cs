2725c45033a4d94498cb19348f8bb6ce1ec40d39
Taha-Hasemi <kiziltahaaslan@gmail.com>
2024-12-29 23:13:51 +0330

    feat: chandelier topic

---
+++ SmartHomeServer/MQTT/ChandelierTopic.cs
@@ -0,0 +1,32 @@
+﻿using Business.Abstract;
+using Entities.Enums;
+using SmartHomeServer.MQTT.Abstract;
+using System.Text;
+using Telegram.Bot;
+
+namespace SmartHomeServer.MQTT;
+
+[Topic(Topic = "chandelier")]
+public class ChandelierTopic : ITopic
+{
+    ITelegramBotClient _client;
+    IStatService _statService;
+    ILogService _logService;
+
+    public ChandelierTopic(IBotService botService, IStatService statService, ILogService logService)
+    {
+        _client = new TelegramBotClient(botService.GetSelectedBot().Token);
+        _statService = statService;
+        _logService = logService;
+    }
+
+    public void Execute(MQTTMessage message)
+    {
+        if (_statService.Any(message.Sender, message.Topic, Encoding.Default.GetString(message.Payload)))
+            return;
+
+        _statService.ChangeStatAsync(((int)EClient.PublicClient).ToString(), message.Topic, Encoding.Default.GetString(message.Payload));
+
+        _logService.AddAsync(message.Topic, Encoding.Default.GetString(message.Payload), message.Sender);
+    }
+}
