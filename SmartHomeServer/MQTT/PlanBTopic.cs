0a098a83be5d4849b633b943ffd8c06ee9539948
Taha-Hasemi <kiziltahaaslan@gmail.com>
2025-01-22 13:14:19 +0330

    feat: plan b topic

---
+++ SmartHomeServer/MQTT/PlanBTopic.cs
@@ -0,0 +1,35 @@
+﻿using Business.Abstract;
+using Entities.Enums;
+using SmartHomeServer.MQTT.Abstract;
+using System.Text;
+using Telegram.Bot;
+
+namespace SmartHomeServer.MQTT;
+
+[Topic(Topic = "planB")]
+public class PlanBTopic : ITopic
+{
+    ITelegramBotClient _client;
+    IStatService _statService;
+    ILogService _logService;
+    IUserService _userService;
+
+    public PlanBTopic(IBotService botService, IStatService statService, ILogService logService, IUserService userService)
+    {
+        _client = new TelegramBotClient(botService.GetSelectedBot().Token);
+        _statService = statService;
+        _logService = logService;
+        _userService = userService;
+    }
+
+    public void Execute(MQTTMessage message)
+    {
+        if (_statService.FirstOrDefalut(((int)EClient.PublicClient).ToString(), "alarm")?.Value == "1")
+            _userService.GetAdmins().ForEach(e =>
+            {
+                _client.SendMessage(e.ChatId, "alarm: Plan B");
+            });
+
+        _logService.AddAsync(message.Topic, Encoding.Default.GetString(message.Payload), message.Sender);
+    }
+}
